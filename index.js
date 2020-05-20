dc6324adc78f1721b06cfe2b39a6acf3ff0ba238
Joseph Yusufov <jyusufov00@stuy.edu>
2020-05-20 17:00:53 -0400

    added nodemon, still cant figure out directory walking

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
 var express = require('express');
-var cors = require('cors');
-var fs = require('fs'); 
+var cors = require('cors'); var fs = require('fs'); 
 var path = require('path');
-var pathToFiles = './public';
-// var options = {
-//     followLinks: false
-// }
-// var walker = walk.walk(pathToFiles, options);
+var currentDir = process.cwd();
 
-const walk = (dir) => {
-    // get the contents of dir
-    fs.readdir(root, (e, items) => {
-        // for each item in the contents
-        items.forEach((item) => {
-            // get the item path
-            let itemPath = path.join(dir, item);
-            // get the stats of the item
-            fs.stat(itemPath, (e, stats) => {
-                // Just log the item path for now
-                console.log(itemPath);
-                // for now just use stats to find out
-                // if the current item is a dir
-                if (stats.isDirectory()) {
-                    // if so walk that too, by calling this
-                    // method recursively
-                    walk(itemPath);
-                } else {
-                    fs.readFile(itemPath, (e, buff) => {
-                        buff.json().then((jsonData) => {
-                            console.log(jsonData);
-                        })
-                    });
-                }
-            });
-        });
-    });
-};
-var app = express();
+app = express();
 
 app.use(cors())
 app.use(express.static('public'));
@@ -46,7 +13,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/list', (req, res) => {
-    walk();
     res.send("timone");
 });
 
