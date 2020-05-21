9c71a186054d8a93853b231561f80f679f80a199
Joseph Yusufov <jyusufov00@stuy.edu>
2020-05-20 23:08:06 -0400

    working list endpoint

--- index.js
+++ index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var cors = require('cors'); var fs = require('fs'); 
 var path = require('path');
+var fs = require('fs');
 var currentDir = process.cwd();
 
 app = express();
+
+const getAllFiles = (dirPath, arrayOfFiles) => {
+    const files = fs.readdirSync(dirPath);
+    arrayOfFiles = arrayOfFiles || [];
+
+    files.forEach((file) => {
+	if (fs.statSync(dirPath + "/" + file).isDirectory()){
+	    arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);    
+	} else {
+	    arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));  
+	};
+    });
+
+    arrayOfFiles.filter((filePath) => {
+	return filePath.slice(-5) === "json";
+    });
+
+    return arrayOfFiles;
+};
+
+const filterAndProcess = (filePathsArray) => {
+    return filePathsArray.filter((filePath) => {
+	return filePath.slice(-4) === "json";
+    }).map((filePath) => {
+	let buff = fs.readFileSync(filePath, 'utf8');
+	return JSON.parse(buff);
+    });
+};
 
 app.use(cors())
 app.use(express.static('public'));
@@ -13,7 +42,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/list', (req, res) => {
-    res.send("timone");
+    const fileNames = filterAndProcess(getAllFiles("public")); 
+    console.log(fileNames);
+    res.send(fileNames);
 });
 
 app.listen(9000, () => {
