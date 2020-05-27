7aab5fddb345de6ff1b810b184f97f3defbee787
Ubuntu <ubuntu@ip-172-31-33-173.us-east-2.compute.internal>
2020-05-27 21:43:20 +0000

    api.bitwise.com is now SSL encrypted

--- index.js
+++ index.js
@@ -1,66 +1,18 @@
+var vhost = require('vhost');
 var express = require('express');
-var cors = require('cors'); var fs = require('fs'); 
-var path = require('path');
+var https = require('https');
 var fs = require('fs');
 
-// var https = require('https');
-// var currentDir = process.cwd();
+// .use(vhost('api.readbitwise.com', require('./api/index.js').app))
+// .listen(80)
+const app = express();
+app.use(vhost('api.readbitwise.com', require('./api/index.js').app));
 
-app = express();
-app.use(cors())
-app.use(express.static('public', { dotfiles: 'allow' }));
-
-const getAllFiles = (dirPath, arrayOfFiles) => {
-    const files = fs.readdirSync(dirPath);
-    arrayOfFiles = arrayOfFiles || [];
-
-    files.forEach((file) => {
-	if (fs.statSync(dirPath + "/" + file).isDirectory()){
-	    arrayOfFiles = getAllFiles(dirPath + '/' + file, arrayOfFiles);    
-	} else {
-	    arrayOfFiles.push(path.join(__dirname, dirPath, "/", file));  
-	};
-    });
-
-    arrayOfFiles.filter((filePath) => {
-	return filePath.slice(-5) === "json";
-    });
-
-    return arrayOfFiles;
-};
-
-const filterAndProcess = (filePathsArray) => {
-    return filePathsArray.filter((filePath) => {
-	return filePath.slice(-4) === "json";
-    }).map((filePath) => {
-	let buff = fs.readFileSync(filePath, 'utf8');
-	return JSON.parse(buff);
-    });
-};
-
-
-app.get('/', (req, res) => {
-    res.send('An alligator approaches!');
-});
-
-app.get('/list', (req, res) => {
-    const fileNames = filterAndProcess(getAllFiles("public")); 
-    console.log(fileNames);
-    res.send(fileNames);
-});
-
-
-
-
-// https.createServer({
-// 	key: fs.readFileSync('/etc/letsencrypt/live/josephyusufov.me/privkey.pem'),
-// 	cert: fs.readFileSync('/etc/letsencrypt/live/josephyusufov.me/fullchain.pem')
-// }, app).listen(443, () => {
-//     console.log('motiv8 listening on port 443')
-// })
-
-// exports.app = app
-
-app.listen(80, () => {
-	console.log("listenint");
+https.createServer({
+     key: fs.readFileSync('/etc/letsencrypt/live/api.readbitwise.com/privkey.pem'),
+     cert: fs.readFileSync('/etc/letsencrypt/live/api.readbitwise.com/fullchain.pem')
+}, app)
+.listen(443, () => {
+    console.log('motiv8 listening on port 443')
 })
+
