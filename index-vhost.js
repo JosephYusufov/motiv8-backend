280b2069c2aadd8f44c9fb64b0451aec88705cf0
Ubuntu <ubuntu@ip-172-31-33-173.us-east-2.compute.internal>
2020-05-28 00:57:51 +0000

    backend ready for first use: :

--- index-vhost.js
+++
@@ -1,7 +0,0 @@
-var vhost = require('vhost');
-var express = require('express');
-
-express()
-.use(vhost('api.18.220.185.98', require('./api/index.js').app))
-// .use(vhost('localhost:9000', require('/path/to/sync').app))
-.listen(80)
--- public/dummy1/index.md
+++
@@ -1,2 +0,0 @@
-# Why I Love New York City
-New York city is absolutely, undoubtedly the best city in the world.
